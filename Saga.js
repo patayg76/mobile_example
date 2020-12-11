e1069498501689b0e75c0621cef8c34f2e0c4ee8
yuriydev214 <69816976+yuriydev214@users.noreply.github.com>
2020-12-11 11:35:13 +0300

    Create Saga.js

---
+++ Saga.js
@@ -0,0 +1,5 @@
+<Issue>
+  Can we discuss via skype for better communication?
+  It is violation of TC to discuss outside payment or communication at this site.
+  My skype address is jnx_team@outlook.com
+</Issue>
